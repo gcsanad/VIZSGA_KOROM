fff70eb8823756a41313e30297d2a6fd3ff43a20
gcsanad <71721197+gcsanad@users.noreply.github.com>
2025-04-04 08:46:09 +0200

    Add files via upload

---
+++ autoform/autoform/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VIZSGA_KOROM_KONZOL;
+
+namespace autoform;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    List<Auto> autok = [];
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        
+
+       
+    }
+
+    private void btnBeolvas_Click(object sender, RoutedEventArgs e)
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+        if (ofd.ShowDialog() == true)
+        {
+            try
+            {
+                autok = Auto.Beolvas(ofd.FileName);
+                dgAutok.ItemsSource = autok;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Rossz fájlt adott meg!");
+            }
+        }
+    }
+
+    private void tbEv_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        lbAutoAdatok.Items.Clear();
+        foreach (var item in autok.Where(x => x.GyartasiEv == tbEv.Text).ToList())
+        {
+            lbAutoAdatok.Items.Add($"{item.Marka} - {item.Modell}");
+        }   
+    }
+}
---
+++ autoform/autoform/Models/Auto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIZSGA_KOROM_KONZOL
+{
+    class Auto
+    {
+        int sorszam;
+        string marka, modell;
+        string gyartasiEv;
+        string szin;
+        int eladottDb;
+        int atlagAr;
+
+        public Auto(string sor)
+        {
+            string[] adatok = sor.Split(';');
+
+            this.Sorszam = int.Parse(adatok[0]);
+            this.Marka = adatok[1];
+            this.Modell = adatok[2];
+            this.GyartasiEv = adatok[3];
+            this.Szin = adatok[4];
+            this.EladottDb = int.Parse(adatok[5]);
+            this.AtlagAr = int.Parse(adatok[6]);
+        }
+
+        public int Sorszam { get => sorszam; set => sorszam = value; }
+        public string Marka { get => marka; set => marka = value; }
+        public string Modell { get => modell; set => modell = value; }
+        public string GyartasiEv { get => gyartasiEv; set => gyartasiEv = value; }
+        public string Szin { get => szin; set => szin = value; }
+        public int EladottDb { get => eladottDb; set => eladottDb = value; }
+        public int AtlagAr { get => atlagAr; set => atlagAr = value; }
+        public static List<Auto> Beolvas(string fajl)
+        {
+            return File.ReadAllLines(fajl).Skip(1).Select(x => new Auto(x)).ToList();
+        }
+    }
+}
